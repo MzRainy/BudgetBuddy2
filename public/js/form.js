7363338fff54e81e9c086caa973ef61ae47b487c
Rainy <142264832+MzRainy@users.noreply.github.com>
2023-12-10 20:04:59 -0600

    updated handlers

--- public/js/form.js
+++ public/js/form.js
@@ -2,7 +2,7 @@
 const express = require('express')
 const router = express.Router()
 
-const homeRoutes = require('../../controllers/homeRoutes');
+const homeRoutes = require('../../controllers/form');
 
 // Set up routes
 router.get('/', homeRoutes.getRecords);
@@ -11,7 +11,7 @@ router.post('/', homeRoutes.postRecord);
 router.get('/:category', homeRoutes.getRecords);
 
 router.get('/:id/edit', homeRoutes.editRecord);
-router.put('/:id', rhomeRoutes.putRecord);
+router.put('/:id', homeRoutes.putRecord);
 router.delete('/:id', homeRoutes.deleteRecord);
 
 // Export router
--- utils/helpers.js
+++ utils/helpers.js
@@ -1,10 +1,26 @@
-module.exports = {
-  format_time: (date) => {
-    return date.toLocaleTimeString();
-  },
-  format_date: (date) => {
-    return `${new Date(date).getMonth() + 1}/${new Date(date).getDate()}/${
-      new Date(date).getFullYear() + 5
-    }`;
-  },
-};
+const Handlebars = require('handlebars')
+
+Handlebars.registerHelper('getIcon', function (category, options) {
+  return category === 'Household' ? `<i class="fas fa-home text-danger"></i>`
+    : category === 'transportation' ? `<i class="fas fa-shuttle-van text-primary"></i>`
+      : category === 'Entertainment' ? `<i class="fas fa-grin-beam text-purple"></i>`
+        : category === 'Dining Out' ? `<i class="fas fa-utensils text-orange"></i>`
+          : `<i class="fas fa-pen text-success"></i>`
+})
+
+Handlebars.registerHelper('isSelected', function (selectedOption, thisOption, options) {
+  return (selectedOption === thisOption) ? 'selected' : ''
+})
+
+Handlebars.registerHelper('getDisplayDate', function (date, options) {
+  const year = date.getFullYear()
+  const month = date.getMonth() + 1
+  const day = date.getDate()
+  return `${year}/${month}/${day}`
+})
+
+Handlebars.registerHelper('getInputDate', function (date, options) {
+  if (!date) { return '' }
+  const month = date.getMonth() === 11 ? 1 : date.getMonth() + 1
+  return `${date.getFullYear()}-${month.toString().padStart(2, '0')}-${date.getDate().toString().padStart(2, '0')}`
+})
